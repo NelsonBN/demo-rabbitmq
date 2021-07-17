f94d8e606dd7b7f1d763660cdb39afece64eb44e
NelsonBN <technobre@gmail.com>
2021-07-17 04:14:21 +0100

    Kick start

---
+++ src/ConsumerAPP/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Demo.RabbitMQ.ConsumerAPP
+{
+    public class Program
+    {
+        static void Main(string[] _)
+        {
+            var consumerId = Guid.NewGuid();
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "myqueue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (_, eventArgs) =>
+                {
+                    var body = eventArgs.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine($"--> Consume {consumerId} Received {message}");
+                };
+
+                channel.BasicConsume(
+                    queue: "myqueue",
+                    autoAck: true,
+                    consumer: consumer
+                );
+
+                Console.WriteLine($"ConsumerAPP {consumerId} runing...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/ProduceAPP/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Demo.RabbitMQ.ProduceAPP
+{
+    public class Program
+    {
+        private const int TIME_PAUSE = 10;
+        static void Main(string[] _)
+        {
+            var produceId = Guid.NewGuid();
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "myqueue",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+
+                var messageId = 0;
+                while(true)
+                {
+                    messageId++;
+                    var message = $"Produce {produceId} - {messageId} > I am Nelson Nobre";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: string.Empty,
+                        routingKey: "myqueue",
+                        basicProperties: null,
+                        body: body
+                    );
+
+                    Console.WriteLine("--> Sent {0}", message);
+
+                    if(TIME_PAUSE > 0)
+                    {
+                        Console.WriteLine($"\tWaiting {TIME_PAUSE}ms...");
+                        Thread.Sleep(TIME_PAUSE);
+                    }
+                }
+            }
+
+            Console.WriteLine("ProduceAPP runing...");
+        }
+    }
+}
